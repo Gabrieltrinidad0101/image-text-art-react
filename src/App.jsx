0d7077962f14f6572ca352466489cd95500ba3f8
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2022-12-10 19:53:52 -0400

    add grid in the new design

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import functionWait from './functionWait';
 import "./app.css"
 const canvas = new Canvas();
 export default function App() {
-    const refCanvas = useRef();
+    //const refCanvas = useRef();
     const [textArtHtml, setTextArtHtml] = useState("");
-    const [letterCss, setLetterCss] = useState({fontSize: 10, spaceLetter: 0});
-    useEffect(_ => { canvas.set = refCanvas.current }, [])
+    const [letterCss, setLetterCss] = useState({ fontSize: 10, spaceLetter: 0 });
+    //useEffect(_ => { canvas.set = refCanvas.current }, [])
 
     const selectImage = async e => {
+        return;
         const reader = new FileReader();
         const srcImage = new Image();
         reader.readAsDataURL(e.target.files[0])
@@ -20,29 +21,41 @@ export default function App() {
     }
 
     const convert = _ => {
+        return;
         const textArt = canvas.convertToText();
         const heigth = textArt.split("\n").length;
         const fontSize = ((window.innerHeight / 2) / heigth);
-        setLetterCss(prev =>({...prev,fontSize}));
+        setLetterCss(prev => ({ ...prev, fontSize }));
         setTextArtHtml(textArt);
     }
+
+    return (
+        <>
+            <div className='app'>
+                <div className='menu-container'>
+                    <div className='menu'>
+                        <div className="button-wrap">
+                            <div className="button-style button-79 background-green">Upload Image</div>
+                            <input id="upload" type="file" onChange={selectImage} />
+                        </div>
+                        <button className="convert button-style button-79" onClick={convert} role="button">convert</button>
+                        <button className='draw button-style' onClick={convert}>Draw</button>
+                    </div>
+                </div>
+                <div className='image-container'></div>
+                <div className='text-art-container'></div>
+            </div>
+        </>
+    )
 
     return (
         <div className='app'>
             <div className='tools'>
                 <canvas ref={refCanvas}></canvas>
-                <div className='buttons' >
-                    <div className="button-wrap">
-                        <div className="button-style button-79 background-green">Upload Image</div>
-                        <input id="upload" type="file" onChange={selectImage} />
-                    </div>
-                    <button className="convert button-style button-79" onClick={convert} role="button">convert</button>
-                    <button className='draw button-style' onClick={convert}>Draw</button>
-                </div>
             </div>
             <div className='container-text-art'>
                 <div>
-                    <pre className='text-art' style={{fontSize: `${letterCss.fontSize}px`, lineHeight: `${letterCss.fontSize}px`}} dangerouslySetInnerHTML={{__html: textArtHtml }}></pre>
+                    <pre className='text-art' style={{ fontSize: `${letterCss.fontSize}px`, lineHeight: `${letterCss.fontSize}px` }} dangerouslySetInnerHTML={{ __html: textArtHtml }}></pre>
                 </div>
             </div>
         </div>
