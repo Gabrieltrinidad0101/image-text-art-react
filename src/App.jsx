fe1b1926c22c07d8c9a4d5f1947789062a17dc76
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2022-12-11 18:07:25 -0400

    width error when generating text art

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import functionWait from './functionWait';
 import "./app.css"
 const canvas = new Canvas();
 export default function App() {
-    //const refCanvas = useRef();
+    const refCanvas = useRef();
+    const refTextArt = useRef();
     const [textArtHtml, setTextArtHtml] = useState("");
-    const [letterCss, setLetterCss] = useState({ fontSize: 10, spaceLetter: 0 });
-    //useEffect(_ => { canvas.set = refCanvas.current }, [])
+    const [letterCss, setLetterCss] = useState({ fontSize: 10,mul: 1 });
+    useEffect(_ => { canvas.set = refCanvas.current }, [])
 
     const selectImage = async e => {
-        return;
         const reader = new FileReader();
         const srcImage = new Image();
         reader.readAsDataURL(e.target.files[0])
@@ -21,12 +21,42 @@ export default function App() {
     }
 
     const convert = _ => {
-        return;
         const textArt = canvas.convertToText();
         const heigth = textArt.split("\n").length;
-        const fontSize = ((window.innerHeight / 2) / heigth);
-        setLetterCss(prev => ({ ...prev, fontSize }));
+        const width = textArt.split("\n")[0].length;
+        const lineHeight = (refTextArt.current.clientHeight / heigth);
+        const lineWidth = ((refTextArt.current.clientWidth) / width);
+        const fontSize = lineHeight > lineWidth ? lineWidth : lineHeight;       
+        console.log({ 
+            fontSize: fontSize,
+            letterSpacing:0,lineHeight,
+            lineaLengthWidth: width,
+            container: refCanvas.current.clientWidth
+
+        }) 
+        setLetterCss(prev => ({ 
+            ...prev, 
+            fontSize: fontSize,
+            letterSpacing:0,lineHeight,
+            lineaLengthWidth: width 
+        }));
         setTextArtHtml(textArt);
+    }
+
+    useEffect(_=>{
+        // const textArtWidth = document.querySelector("pre").getBoundingClientRect().width;
+        // const canvasWidth = refCanvas.current.clientWidth;
+        // const diff = Math.floor(textArtWidth) - Math.floor(canvasWidth);  
+        // console.log(Math.floor(textArtWidth) ,"  ", Math.floor(canvasWidth))
+        // if(diff <= 1 || diff >= 0) return;
+        // console.log("ok")
+        // setLetterCss(prev =>({...prev,mul: prev.mul + 0.1}))
+    },[letterCss])
+
+
+    const styles = {
+        fontSize: `${letterCss.fontSize}px`,
+        lineHeight: `${letterCss.lineHeight}px`,
     }
 
     return (
@@ -34,30 +64,40 @@ export default function App() {
             <div className='app'>
                 <div className='menu-container'>
                     <div className='menu'>
-                        <div className="button-wrap">
-                            <div className="button-style button-79 background-green">Upload Image</div>
-                            <input id="upload" type="file" onChange={selectImage} />
+                        <div className="file-upload-container">
+                            <div className="file-upload-label button-style">Upload Image</div>
+                            <input className="file-upload" type="file" onChange={selectImage} />
                         </div>
                         <button className="convert button-style button-79" onClick={convert} role="button">convert</button>
                         <button className='draw button-style' onClick={convert}>Draw</button>
                     </div>
                 </div>
-                <div className='image-container'></div>
-                <div className='text-art-container'></div>
+                <div className='image-container'>
+                    <div className='image'>
+                        <div>
+                            <h2 className='title-text'>
+                                Image
+                            </h2>
+                        </div>
+                        <canvas ref={refCanvas} width="100px"></canvas>
+                    </div>
+                </div>
+                <div className='text-art-container'>
+                    <div className='text-art'>
+                        <div>
+                            <h2 className='title-text'>
+                                Text Art
+                            </h2>
+                        </div>
+                        <div className='text' ref={refTextArt}>
+                            <pre
+                                style={styles} 
+                                dangerouslySetInnerHTML={{ __html: textArtHtml }}></pre>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
-
-    return (
-        <div className='app'>
-            <div className='tools'>
-                <canvas ref={refCanvas}></canvas>
-            </div>
-            <div className='container-text-art'>
-                <div>
-                    <pre className='text-art' style={{ fontSize: `${letterCss.fontSize}px`, lineHeight: `${letterCss.fontSize}px` }} dangerouslySetInnerHTML={{ __html: textArtHtml }}></pre>
-                </div>
-            </div>
-        </div>
-    )
 }
+
--- src/canvas.js
+++ src/canvas.js
@@ -13,15 +13,34 @@ class Canvas{
 
     convertToText(){
         const image = this.context.getImageData(0,0,this.canvas.width,this.canvas.height).data;  
-        let breakLine = 0;
+        let breakLine = 1;
         let res = "";
         for(let i = 0; i < image.length; i += 4){
-            const color = image[i] + image[i+1] + image[i+2] + image[i+3] / 4 / 255;
-            res += color >= 1 ? "0" : "-";
+            const color = (image[i] + image[i+1] + image[i+2] + image[i+3]) / 4;
+            res += this.#getCharacterOfPixel(color);
             if(breakLine == this.canvas.width){res += "\n";breakLine = 0; };
             ++breakLine;
         }
         return res
+    }
+
+    #getCharacterOfPixel(g){
+        if(g == 255) return "$"
+        if(g >= 250) return "@"
+        if(g >= 220) return "*"
+        if(g >= 200) return "+"
+        if(g >= 180) return "#"
+        if(g >= 160) return "&"
+        if(g >= 140) return "%"
+        if(g >= 120) return "∈"
+        if(g >= 110) return "∇"
+        if(g >= 100) return ":"
+        if(g >= 80) return "£"
+        if(g >= 60) return "/"
+        if(g >= 40) return "("
+        if(g >= 20) return "!"
+        if(g >= 10) return "∀"
+        return " "
     }
 }
 
